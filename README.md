595320eb5af06492371a2a38c3b0affe3ae5d6e6
Jacob <41343814+McSqueelibob@users.noreply.github.com>
2021-01-22 15:54:25 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# gamechanger-season
+ Main code base for FRC#2478 in the 2021 season
---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+/**
+ * All <i>actual</i> constants are here, field descriptions or things about the code that don't change.
+ */
+public final class Constants {
+    
+    public static final int PRIMARY_PID = 0; // primary pid ids
+    public static final int AUXILARY_PID = 1; // auxilary pid ids
+    public static final int MS_TIMEOUT = 10; // 10 ms before a talon config fails
+
+}
