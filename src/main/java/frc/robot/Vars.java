9cb82423e8a1efea8b731f47da70ca9faea65d92
Jacob <41343814+McSqueelibob@users.noreply.github.com>
2021-02-12 16:39:55 -0700

    Update Vars.java

--- src/main/java/frc/robot/Vars.java
+++ src/main/java/frc/robot/Vars.java
@@ -121,8 +121,8 @@ public class Vars {
   
   public static final double DRIVE_MAX_M_PER_S = 189.72441; // in/s
   public static final double DRIVE_MAX_M_PER_S_SQUARED = 1743; // in/s^2
-  public static final double AUTO_MAX_M_PER_S = 118; // in/s
-  public static final double AUTO_MAX_M_PER_S_SQUARED = 67; // in/s^2
+  public static final double AUTO_MAX_M_PER_S = 12; // in/s  118
+  public static final double AUTO_MAX_M_PER_S_SQUARED = 12; // in/s^2  67
 
   // Reasonable baseline values for a RAMSETE follower in units of meters and seconds
   public static final double RAMSETE_B = 2;
