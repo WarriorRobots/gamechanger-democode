77ebae09504f2241327080a50fcae678e3bb5211
WestwoodRobots <mesawestwoodrobotics@gmail.com>
2021-02-26 16:11:16 -0700

    Revert "AutoBounce() path corrected"
    This reverts commit 98e25450aa3661814e21bcb2cf34c920b3b024d8.

--- src/main/java/frc/robot/commands/auto/trajectories/TBounceFour.java
+++ src/main/java/frc/robot/commands/auto/trajectories/TBounceFour.java
@@ -38,8 +38,8 @@ public class TBounceFour extends TBase {
     */
     start = new Pose2d(Units.inchesToMeters(0), Units.inchesToMeters(0), Rotation2d.fromDegrees(0));
     Waypoints.add(new Translation2d(Units.inchesToMeters(-30), Units.inchesToMeters(0)));
-    Waypoints.add(new Translation2d(Units.inchesToMeters(-60), Units.inchesToMeters(-30)));
-    end = new Pose2d(Units.inchesToMeters(-60), Units.inchesToMeters(-60), Rotation2d.fromDegrees(90));
+    Waypoints.add(new Translation2d(Units.inchesToMeters(-60), Units.inchesToMeters(30)));
+    end = new Pose2d(Units.inchesToMeters(-60), Units.inchesToMeters(60), Rotation2d.fromDegrees(90));
   }
 
 }
